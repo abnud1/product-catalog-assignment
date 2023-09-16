3799b0df84b568da3df0dc4855ac6c25e89c8e00
abnud1 <ahmad13932013@hotmail.com>
2023-09-16 12:47:39 +0300

    initial commit

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires, unicorn/prefer-module
+const nextJest = require("next/jest") as Awaited<
+  typeof import("next/jest")
+>["default"];
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: "./",
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+};
+
+// eslint-disable-next-line unicorn/prefer-module
+module.exports = createJestConfig(customJestConfig);
---
+++ jest.setup.ts
@@ -0,0 +1,3 @@
+// Learn more: https://github.com/testing-library/jest-dom
+// eslint-disable-next-line unicorn/prefer-module
+require("@testing-library/jest-dom");
